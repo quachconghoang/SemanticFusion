8beed417007d7d745dff81df5599005517fd1cf8
hoangqc <quachconghoang89@gmail.com>
2021-05-04 01:25:27 +0700

    Players
    ... fast & orb

--- Playground/test_stereo.py
+++ Playground/test_stereo.py
@@ -1,1 +1,37 @@
-# from Datasets.bag_loader import
+import sys
+sys.path.append('../')
+from Datasets.bag_loader import RosBagLoader
+from config import TUMVI, VIODE
+import numpy as np
+import cv2 as cv
+
+if __name__ == "__main__":
+    # print(VIODE)
+    loader = RosBagLoader(config_dict=VIODE)
+    loader.loadBag()
+    l_img, r_img, odom = loader.getImg(100)
+
+#FAST
+    # fast = cv.FastFeatureDetector_create()
+    # kp = fast.detect(l_img, None)
+    # img2 = cv.drawKeypoints(l_img, kp, None, color=(255, 0, 0))
+    # # Print all default params
+    # print("Threshold: {}".format(fast.getThreshold()))
+    # print("nonmaxSuppression:{}".format(fast.getNonmaxSuppression()))
+    # print("neighborhood: {}".format(fast.getType()))
+    # print("Total Keypoints with nonmaxSuppression: {}".format(len(kp)))
+
+#ORB
+
+    # Initiate ORB detector
+    orb = cv.ORB_create()
+    kp = orb.detect(l_img,None)
+    kp, des = orb.compute(l_img, kp)
+    img2 = cv.drawKeypoints(l_img, kp, None, color=(0,255,0), flags=0)
+
+    # img = np.concatenate((l_img, r_img), axis=1)
+    # cv.imshow('***', img2)
+    # cv.waitKey()
+    # cv.destroyAllWindows()
+
+    # loader
---
+++ config.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+VIODE = {
+    'msg': ['/cam0/image_raw', '/cam1/image_raw', '/cam0/segmentation', '/cam1/segmentation',
+             '/imu0', '/odometry'],
+    'img_size': [752,480],
+    'img_type' : 'bgr8',
+    'folder': str(Path.home())+ '/Datasets/VIODE/city_day/',
+    'files': ['0_none.bag', '1_low.bag', '2_mid.bag', '3_high.bag'],
+    'test_path': str(Path.home())+ '/Datasets/VIODE/city_day/0_none.bag',
+    'test_mode': True
+}
+
+TUMVI = {
+    'msg': ['/cam0/image_raw', '/cam1/image_raw',
+           '/imu0', '/vrpn_client/raw_transform'],
+    'img_size': [512,512],
+    'img_type' : 'mono16',
+    'folder': str(Path.home())+ '/Datasets/TUM_VI/',
+    'files': ['dataset-corridor1_512_16.bag'],
+    'test_path': str(Path.home())+ '/Datasets/TUM_VI/dataset-corridor1_512_16.bag',
+    'test_mode': True
+}
+
